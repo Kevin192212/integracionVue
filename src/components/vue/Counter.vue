eaae6dae91940257e2942fc5552b06fb2b744ffd
Kevin192212 <kppardo0401@gmail.com>
2024-04-25 13:01:57 -0500

    integration vue

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import { ref } from 'vue'
+    let count = ref(0);
+
+    function addvalues(){
+        count.value++;
+        console.log(count)
+    }
+</script>
+
+<template>
+    <div @click="addvalues()" class="bg-blue-500 text-white py-2 px-4 rounded inline-block hover:cursor-pointer hover:bg-blue-600 transition-colors">
+        ¡Touch! {{ count }}
+    </div>
+</template>
